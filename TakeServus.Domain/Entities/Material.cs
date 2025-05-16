82889d1435db2d8bcd6147b465165a941af4f4ad
Mustafa Ünal <mustafaunal@Mustafas-MacBook-Air.local>
2025-05-16 18:48:53 +0300

    WIP: Local changes to Material entity

--- TakeServus.Domain/Entities/Material.cs
+++ TakeServus.Domain/Entities/Material.cs
@@ -1,11 +1,12 @@
+using TakeServus.Domain.Common;
+
 namespace TakeServus.Domain.Entities;
 
-public class Material
+public class Material : BaseEntityWithAudit
 {
-    public Guid Id { get; set; }
     public string Name { get; set; } = default!;
+    public string Unit { get; set; } = default!;
     public decimal UnitPrice { get; set; }
     public int StockQuantity { get; set; }
-
     public ICollection<JobMaterial> JobMaterials { get; set; } = new List<JobMaterial>();
 }
